406fa3c980b095bb31e4753631717e46f051324f
polarizeproject <107323117+polarizeproject@users.noreply.github.com>
2022-06-11 22:38:02 +0200

    up

---
+++ sections/sound/js/script.js
@@ -0,0 +1,161 @@
+// primo gruppo
+
+var button = document.getElementById("uno");
+var audio = document.getElementById("audio1");
+
+button.addEventListener("click", function(){
+    document.getElementById("uno").classList.toggle("bg-none");
+  if(audio.paused){
+    audio.play();
+  } else {
+    audio.pause(); 
+  }
+});
+
+
+var button2 = document.getElementById("due");
+var audio2 = document.getElementById("audio2");
+
+button2.addEventListener("click", function(){
+    document.getElementById("due").classList.toggle("bg-none");
+  if(audio2.paused){
+    audio2.play();
+  } else {
+    audio2.pause(); 
+  }
+});
+
+
+var button3 = document.getElementById("tre");
+var audio3 = document.getElementById("audio3");
+
+button3.addEventListener("click", function(){
+    document.getElementById("tre").classList.toggle("bg-none");
+  if(audio3.paused){
+    audio3.play();
+  } else {
+    audio3.pause(); 
+  }
+});
+
+
+var button4 = document.getElementById("quattro");
+var audio4 = document.getElementById("audio4");
+
+button4.addEventListener("click", function(){
+    document.getElementById("quattro").classList.toggle("bg-none");
+  if(audio4.paused){
+    audio4.play();
+  } else {
+    audio4.pause(); 
+  }
+});
+
+// secondo gruppo
+
+
+var button5 = document.getElementById("cinque");
+var audio5 = document.getElementById("audio5");
+
+button5.addEventListener("click", function(){
+    document.getElementById("cinque").classList.toggle("bg-none");
+  if(audio5.paused){
+    audio5.play();
+  } else {
+    audio5.pause(); 
+  }
+});
+
+
+var button6 = document.getElementById("sei");
+var audio6 = document.getElementById("audio6");
+
+button6.addEventListener("click", function(){
+    document.getElementById("sei").classList.toggle("bg-none");
+  if(audio6.paused){
+    audio6.play();
+  } else {
+    audio6.pause(); 
+  }
+});
+
+
+var button7 = document.getElementById("sette");
+var audio7 = document.getElementById("audio7");
+
+button7.addEventListener("click", function(){
+    document.getElementById("sette").classList.toggle("bg-none");
+  if(audio7.paused){
+    audio7.play();
+  } else {
+    audio7.pause(); 
+  }
+});
+
+
+var button8 = document.getElementById("otto");
+var audio8 = document.getElementById("audio8");
+
+button8.addEventListener("click", function(){
+    document.getElementById("otto").classList.toggle("bg-none");
+  if(audio8.paused){
+    audio8.play();
+  } else {
+    audio8.pause(); 
+  }
+});
+
+
+// terzo gruppo
+
+
+var button9 = document.getElementById("nove");
+var audio9 = document.getElementById("audio9");
+
+button9.addEventListener("click", function(){
+    document.getElementById("nove").classList.toggle("bg-none");
+  if(audio9.paused){
+    audio9.play();
+  } else {
+    audio9.pause(); 
+  }
+});
+
+
+var button10 = document.getElementById("dieci");
+var audio10 = document.getElementById("audio10");
+
+button10.addEventListener("click", function(){
+    document.getElementById("dieci").classList.toggle("bg-none");
+  if(audio10.paused){
+    audio10.play();
+  } else {
+    audio10.pause(); 
+  }
+});
+
+
+var button11 = document.getElementById("undici");
+var audio11 = document.getElementById("audio11");
+
+button11.addEventListener("click", function(){
+    document.getElementById("undici").classList.toggle("bg-none");
+  if(audio11.paused){
+    audio11.play();
+  } else {
+    audio11.pause(); 
+  }
+});
+
+
+var button12 = document.getElementById("dodici");
+var audio12 = document.getElementById("audio12");
+
+button12.addEventListener("click", function(){
+    document.getElementById("dodici").classList.toggle("bg-none");
+  if(audio12.paused){
+    audio12.play();
+  } else {
+    audio12.pause(); 
+  }
+});
